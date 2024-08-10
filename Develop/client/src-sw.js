b13b6bfa8265b35d4e7ae3db5fe92c917d6a0724
Eric <ekellogg90@gmail.com>
2024-08-10 17:09:17 -0500

    src-sw added

--- Develop/client/src-sw.js
+++ Develop/client/src-sw.js
@@ -26,5 +26,8 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
-registerRoute();
+// xTODO: Implement asset caching
+registerRoute(
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  pageCache
+);
